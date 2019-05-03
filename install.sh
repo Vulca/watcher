adc77d4d4183fff69becf29d5e066a0164e548f8
Pavel Stratil <pavel@vaizard.org>
2019-05-03 16:10:41 +0000

    commenting out the installer parts that don't have actual files to them yet

--- install.sh
+++ install.sh
@@ -26,31 +26,31 @@ eecho ""
 eecho "--- Pulling from github"
 git clone https://github.com/terminusfoundation/watcher.git .
 
-eecho "--- Testing if phinx.yml production environment is set up"
-if [ ! -f ./phinx.yml ]; then
-  eexit("phinx.yml is not configured yet. please copy phinx.yml.dist to phinx.yml and set it up.")
-fi
+#eecho "--- Testing if phinx.yml production environment is set up"
+#if [ ! -f ./phinx.yml ]; then
+#  eexit("phinx.yml is not configured yet. please copy phinx.yml.dist to phinx.yml and set it up.")
+#fi
 
-[[ $(php vendor/bin/phinx test -e production | grep 'success') ]] || eexit "phinx.yml isn't configured correctly"
+#[[ $(php vendor/bin/phinx test -e production | grep 'success') ]] || eexit "phinx.yml isn't configured correctly"
 
-eecho "--- phinx.yml works, ensure it is assumed unchanged by git"
-git update-index --assume-unchanged phinx.yml
+#eecho "--- phinx.yml works, ensure it is assumed unchanged by git"
+#git update-index --assume-unchanged phinx.yml
 
 eecho "--- Ensuring fresh dependencies"
 rm -rf ./vendor/*
 composer install
 
-eecho "--- Testing if settings.php is set up"
-if [ ! -f ./config.php ]; then
-  eexit("config.php is not configured yet. please copy config.php.dist to config.php and set it up.")
-fi
+#eecho "--- Testing if settings.php is set up"
+#if [ ! -f ./config.php ]; then
+#  eexit("config.php is not configured yet. please copy config.php.dist to config.php and set it up.")
+#fi
 
-eecho "\$config = require '_preflight.php';" | php -a
-[[ $(echo "\$config = require '_preflight.php';" | php -a | grep 'ERROR') ]] && eexit "settings.php misconfigured"
+#eecho "\$config = require '_preflight.php';" | php -a
+#[[ $(echo "\$config = require '_preflight.php';" | php -a | grep 'ERROR') ]] && eexit "settings.php misconfigured"
 
 
-eecho "--- Setting up the database"
-php vendor/bin/phinx migrate -e production
+#eecho "--- Setting up the database"
+#php vendor/bin/phinx migrate -e production
 
 eecho ""
 eecho "***********************"
