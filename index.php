b6d2c07f14fab68d67f1ae350f72b4f3a8c090c8
Pavel Stratil <pavel@vaizard.org>
2019-05-09 14:05:44 +0000

    small apifying step

--- index.php
+++ index.php
@@ -2,15 +2,32 @@
 require 'vendor/autoload.php';
 use Goutte\Client;
 
-$url = "http://industra.space";
 $css_selector = "a.title.may-blank";
 $thing_to_scrape = "_text";
 
 $client = new Client();
-$crawler = $client->request('GET', $url);
-$output = $crawler;
-#$output = $crawler->filterXPath('descendant-or-self::body');
-#$output = $crawler->filter($css_selector)->extract($thing_to_scrape);
 
-var_dump($output);
+// TODO: set up an api. Design brainstormer below:
+// VERBS: 
+//  - test: test a scrape (pass `url` and any number of `filter`, `filterxpath`, `extract`, ... methods)
+//  - add: as test, but store scrape configuration in database
+//  - ...
+
+
+$verb = $_GET['verb'];
+if ($verb == 'test') {
+  if ( !filter_has_var( INPUT_GET, 'url') ) { $url = "https://industra.space"; } else {
+  $url = filter_input( INPUT_GET, 'url', FILTER_SANITIZE_URL);
+  $crawler = $client->request('GET', $url);
+  $output = $crawler;
+  //$output = $crawler->filterXPath('descendant-or-self::body');
+  //$output = $crawler->filter($css_selector)->extract($thing_to_scrape);
+  print_r($output->html());
+}}
+else {
+  echo "<h1>BAD VERB</h1>";
+  echo "ALLOWED VERBS: test<br />";
+  echo "EXAMPLE: http://localhost/?verb=test&url=https://google.com";
+}
+
 ?>
